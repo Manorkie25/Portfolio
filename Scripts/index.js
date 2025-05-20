8f999ec9581181723777545d7eed8b0ae634ee6e
Manorkie25 <manorkie96@gmail.com>
2025-05-20 10:18:59 -0700

    NewAdd

---
+++ Scripts/index.js
@@ -0,0 +1,14 @@
+// declare variable to store the navbar element
+const navElement = document.getElementById('navbar');
+// detect scroll event on document 
+document.addEventListener('scroll', function() {
+       // console.log(window.scrollY);
+       if (window.scrollY > 70)  {
+        // change navbar bg to blue
+        navElement.style.backgroundColor= '#0779E4';
+       } else {
+        // change navbar bg to transparent
+       navElement.style.backgroundColor= 'transparent'; 
+       }
+        
+       });
